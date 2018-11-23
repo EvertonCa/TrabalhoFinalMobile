6f4dfcd12f4d180eff61cafecefebd17011f4ace
Lucas Laheras <lucaslaheras@hotmail.com>
2018-11-22 22:12:15 -0200

    Tela inicial com o grafico

--- app/src/main/java/com/example/evertoncardoso/trabalhofinalmobile/View/MainActivity.java
+++ app/src/main/java/com/example/evertoncardoso/trabalhofinalmobile/View/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,11 +15,20 @@ public class MainActivity extends AppCompatActivity {
 
     EditText login;
     EditText password;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chamaMenuPrincipal();
+            }
+        });
+
     }
 
     @Override
@@ -65,6 +75,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void chamaMenuPrincipal()
     {
-        //startActivity(new Intent(this, MenuPrincipal.class));
+        startActivity(new Intent(this, TelaInicialActivity.class));
     }
 }
