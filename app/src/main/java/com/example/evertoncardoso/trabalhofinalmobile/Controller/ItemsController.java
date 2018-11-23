a829c991cfc21a4979dda82e6edcd6f7e1f19bf6
EvertonCa <eve.023@hotmail.com>
2018-11-23 17:46:48 -0200

    Controller de itens

---
+++ app/src/main/java/com/example/evertoncardoso/trabalhofinalmobile/Controller/ItemsController.java
@@ -0,0 +1,11 @@
+package com.example.evertoncardoso.trabalhofinalmobile.Controller;
+
+import com.example.evertoncardoso.trabalhofinalmobile.Database.DataBase;
+
+public class ItemsController {
+
+    public static void criaItem(DataBase db)
+    {
+
+    }
+}
